6ad9333d797b0e8cede1422c456e13c2bb4000ac
Tony Skoglund <antyskoglund@gmail.com>
2025-10-30 14:52:21 -0500

    NIM Game + minMax (need to add parameters to make sure player can't take from an empty stack to win)

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <ctime>
 #include <vector>
 #include <unistd.h>
+#include <algorithm>
 
 using namespace std;
 
@@ -11,48 +12,109 @@ class NimGame{
         string name1,name2 = "";
         vector <int> stacks;
         int remove, stackSelect, pileAmount;
-        bool turn, move;
+        bool turn, move, isMaximizing;
+
+        int minimax(vector<int> stacks, bool isMaximizing) {
+        int score = evaluate(stacks, isMaximizing);
+        if (score != 2)
+            return score;   
+
+        vector<vector<int>> nextStates = possibleNewStates(stacks);
+        vector<int> scores;
+
+        for (auto& newStacks : nextStates) {
+            scores.push_back(minimax(newStacks, !isMaximizing));
+        }
+
+        if (isMaximizing)
+            return *max_element(scores.begin(), scores.end());
+        else
+            return *min_element(scores.begin(), scores.end());
+    }
+
+    vector<vector<int>> possibleNewStates(vector<int> stacks) {
+    vector<vector<int>> result; 
+
+    for (size_t i = 0; i < stacks.size(); ++i) {
+        for (int take = 1; take <= 3 && take <= stacks[i]; ++take) {
+            vector<int> newState = stacks;
+            newState[i] -= take;
+            result.push_back(newState);     
+        }
+    }
+    return result;
+}
+
+    int evaluate(const vector<int>& stacks, bool isMaximizing) {
+        bool allEmpty = all_of(stacks.begin(), stacks.end(), [](int x){ return x == 0; });
+
+        if (allEmpty)
+            return isMaximizing ? 1 : -1;
+        return 2;
+    }
+
+    vector<int> bestMove(vector<int> stacks) {
+        int bestScore = -1000;
+        vector<int> bestStacks = stacks;
+
+        vector<vector<int>> nextStates = possibleNewStates(stacks);
+        for (auto& newStacks : nextStates) {
+            int score = minimax(newStacks, false);
+            if (score > bestScore) {
+                bestScore = score;
+                bestStacks = newStacks;
+            }
+        }
+        return bestStacks;
+    }
+
 
     void generateLists(){
         while (!move){
-            cout << "\nHow many piles would you like? (2-10): ";
+            cout << "\nHow many piles would you like? (2-5): ";
             cin >> pileAmount;
-            move = (pileAmount >= 2 && pileAmount <= 10);
+            move = (pileAmount >= 2 && pileAmount <= 5);
             if (!move){
                 cout << "\nInvalid input, please try again.";
             }
         }
         srand(time(0));
         for (int i = 0; i < pileAmount; i++){
-            int r = rand() % (20) + 5;
+            int r = rand() % (3) + 3;
             stacks.push_back(r);
             sleep(.2);
         }
     }
 
     void displayGame(){
+        int num = 0;
         for (int i = 0; i < stacks.size(); i++){
-            if (stacks[i] <=0){
-                if (turn == true){
-                    cout << "Congrats, " << name1 << "wins!";
-                }else {
-                    cout << "Congrats, " << name2 << "wins!";
-                }
-            exit(0);
-            }else{
+            if (stacks[i] < 0){
+                stacks[i] = 0;
+            }
+        }
+        for (int n: stacks){
+            num+=n;
+        }
+        if (num <= 0){
+            if (turn == true){
+                cout << "Congrats, " << name1 << "wins!";
+            }else {
+                cout << "Congrats, " << name2 << "wins!";
+            }
+        exit(0);
+        }
+        for (int i = 0; i < stacks.size(); i++){
             cout << "Stack " << i+1 << ":";
             for (int j = 0; j < stacks[i]; j++){
                 cout << " | ";
             }
             cout << "(" << stacks[i] << ")\n";
-            }
         }
         makeMove();
     }
 
-    void minMax(){
-        
-    }
+    
     
     void makeMove(){
         if (turn == true){
@@ -65,39 +127,28 @@ class NimGame{
                 cin >> remove;
                 if (remove > 0 && remove < 4 && stackSelect < stacks.size()  && stackSelect >= 0){
                     stacks[stackSelect] -= remove;
-                    displayGame();
                     turn = false;
                     move = true;
-                }else{
-                    cout << "Invalid move, try again" << endl;
-                }
-            }
-        }
-        if (turn == false){
-            move = false;
-            while (!move){
-                cout << name1 << "'s Turn \nWhich stack would you like to remove from? ";
-                cin >> stackSelect;
-                stackSelect -= 1;
-                cout << "\n How many objects would you like to remove 1-3? ";
-                cin >> remove;
-                if (remove > 0 && remove < 4 && stackSelect < stacks.size()  && stackSelect >= 0){
-                    stacks[stackSelect] -= remove;
                     displayGame();
-                    turn = true;
-                    move = true;
                 }else{
                     cout << "Invalid move, try again" << endl;
                 }
             }
         }
+        if (turn == false) {
+            cout << name2 << "'s Turn (AI is thinking...)\n";
+            stacks = bestMove(stacks);
+            turn = true;
+            displayGame();
+        }
+
     }
 
     void gameSetup(){
         cout << "Enter Player Names \n" << endl << "Player One: ";
         cin >> name1;
-        cout << "\nPlayer Two: ";
-        cin >> name2;
+        name2 = "AI Opponent";
+        cout << "\nPlayer Two: " << name2 << endl;
         turn = true;
         generateLists();
         displayGame();
